721d9edb881154338d0742450aab28492e9d4270
RConant6 <rconant6@gmail.com>
2025-08-30 22:22:29 -0400

    initial commit with build for compiler version [14.0]

--- build.zig
+++ build.zig
@@ -5,20 +5,20 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
+    const exe_mod_interp = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const exe = b.addExecutable(.{
-        .name = "zlox",
-        .root_module = exe_mod,
+    const exe_interp = b.addExecutable(.{
+        .name = "zlox_interp",
+        .root_module = exe_mod_interp,
     });
 
-    b.installArtifact(exe);
+    b.installArtifact(exe_interp);
 
-    const run_cmd = b.addRunArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe_interp);
 
     run_cmd.step.dependOn(b.getInstallStep());
 
@@ -26,6 +26,31 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const run_step = b.step("run", "run zlox");
+    const run_step = b.step("runInt", "run zlox interpreter");
     run_step.dependOn(&run_cmd.step);
+
+    //////////////////  FOR PART 2 //////////////////////////
+    const exe_mod_comp = b.createModule(.{
+        .root_source_file = b.path("src_comp/compiler.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe_comp = b.addExecutable(.{
+        .name = "zlox_comp",
+        .root_module = exe_mod_comp,
+    });
+
+    b.installArtifact(exe_comp);
+
+    const run_comp = b.addRunArtifact(exe_comp);
+
+    run_comp.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_comp.addArgs(args);
+    }
+
+    const run_comp_step = b.step("runComp", "run zlox compiler");
+    run_comp_step.dependOn(&run_comp.step);
 }
